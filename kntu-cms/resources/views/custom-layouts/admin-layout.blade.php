eef459997f62eb52b168721e1dbd762b945fa32f
TANK <gholizadeh.pro@gmail.com>
2021-04-30 19:05:37 +0430

    cv Config page template created

---
+++ kntu-cms/resources/views/custom-layouts/admin-layout.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>@yield('title')</title>
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</head>
+<body class="bg-gray-50">
+    <div><!--main wrapper-->
+
+        <div class="flex relative mb-12"><!--Site top-->
+            <div>
+                <nav class="w-full h-12 flex justify-around items-center absolute bg-blue-500 text-gray-50">
+                    <div>
+                        <!--Logo-->
+                        <h2>
+                            KNTU
+                        </h2>
+                    </div>
+
+                    <ul class="flex">
+                        <!--Navbar list-->
+                        <li class="mx-2">
+                            Home
+                        </li>
+                        <li class="mx-2">
+                            Cv Config
+                        </li>
+                        <li class="mx-2">
+                            Poster
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <main>
+            @yield('mainContent')
+        </main>
+
+        <div class="absolute w-full h-52 bg-gray-400 bottom-0">
+            this is the footer
+        </div>
+    </div>
+
+</body>
+</html>
--- kntu-cms/resources/views/welcome.blade.php
+++ kntu-cms/resources/views/welcome.blade.php
@@ -1,15 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>KNTU master</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
-    <div class="border-4 border-red-100 bg-blue-100">
-        hello
-    </div>
-</body>
-</html>
+@extends('custom-layouts.admin-layout')
+
+@section('title', 'Home')
+
+@section('mainContent')
+    <div class="grid grid-cols-4 text-right" dir="rtl">
+        <div class="col-span-1"><!--sidebar-->
+            <nav class="pt-8 mr-6 xl:pr-14">
+                <h3 class="font-bold text-lg">دستبندی های رزومه</h3>
+                <ul class="mr-4 mt-3 font-semibold text-base">
+                    <li class="mt-2 font-light">دسته اول</li>
+                    <li class="mt-2">first cat</li>
+                    <li class="mt-2">second cat</li>
+                    <li class="mt-2">third cat</li>
+                    <button type="button" class="flex justify-end px-3 -mr-1 mt-1 rounded border-2 bg-gray-100">
+                        <span class="-mt-1">category</span>
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                    </button>
+                </ul>
+                <button type="button" class="flex items-center px-10 mt-2 rounded border-2 bg-gray-100" title="Add Category">
+                    <span class="-mt-1">category</span>
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                </button>
+            </nav>
+        </div>
+        <div class="col-span-3">
+            <div class="mr-10 my-6 p-3"><!--cards go here-->
+                <div class="rounded overflow-hidden bg-gray-200 border-b border-gray-300 w-4/5 h-20 mb-6 flex items-center justify-between  shadow">
+                    <div class="text-lg font-semibold items-center pr-8"><!--card head-->
+                        عنوان برای عنصر رزومه یک
+                    </div>
+                    <div class="flex ml-16">
+                        <div>
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+                        </div>
+                        <div>
+                            <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                        </div>
+                        <div>
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="rounded overflow-hidden bg-gray-200 border-b border-gray-300 w-4/5 h-20 mb-6 flex items-center justify-between  shadow">
+                    <div class="text-lg font-semibold items-center pr-8"><!--card head-->
+                        عنوان برای عنصر رزومه یک
+                    </div>
+                    <div class="flex ml-16">
+                        <div>
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+                        </div>
+                        <div>
+                            <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                        </div>
+                        <div>
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                        </div>
+                    </div>
+                </div>
+                <button class="rounded overflow-hidden bg-white w-4/5 h-20 mb-6 flex items-center justify-center shadow">
+                    <svg class="w-14 h-14" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                </button>
+            </div>
+        </div>
+
+    <script>
+
+    </script>
+@endsection
