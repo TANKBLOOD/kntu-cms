1b0e3d5e5801a1ab40b582e27bdbc937300eb078
TANK <gholizadeh.pro@gmail.com>
2021-05-06 03:56:36 +0430

    Bugs on editing new created components fixed.

--- kntu-cms/public/js/cv/editCvAjax.js
+++ kntu-cms/public/js/cv/editCvAjax.js
@@ -153,8 +153,8 @@ function openPureTextEditModal(item) {
 
 }
 
-$("#editPureTextBtn").on("click", editOptionAjax);
-function editOptionAjax(event) {
+$("#editPureTextBtn").on("click", editPureTextAjax);
+function editPureTextAjax(event) {
     event.preventDefault();
 
     let compId= clickedComponent.parentNode.getAttribute('data-com-id');
@@ -290,21 +290,11 @@ function createSmOptionAjax(event) {
         },
         success:function(response){
         if(response) {
-            let newComp= document.createElement('div');
-            newComp.setAttribute('class', 'flex items-center my-4');
-            newComp.innerHTML= '<svg class="w-6 h-6 mt-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path></svg> \
-            <div class="ml-5 text-2xl"><!--sm-option title--> \
-                <span>'+comTitle+'</span>: \
-            </div> \
-            <div class="font-semibold"><!--sm-option value--> '+comValue+' </div> \
-            <div class="mr-8 flex mt-1" data-com-id="'+response['comId']+'"> \
-                <button onclick="openDeleteModal(this)" class="ml-2 px-2 bg-red-500 rounded-md text-white"> \
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg> \
-                </button> \
-                <button onclick="openSmOptionEditModal(this)" class="px-2 bg-blue-500 rounded-md text-white"> \
-                    <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg> \
-                </button> \
-            </div>';
+            let smOptionTemplate= document.getElementsByClassName('sm-option-template')[0];
+            let newComp= smOptionTemplate.cloneNode(true);
+            newComp.childNodes[4].childNodes[2].innerText= comTitle;
+            newComp.childNodes[6].innerText= comValue;
+            newComp.childNodes[8].setAttribute('data-com-id', response['comId']);
             componentHolder.appendChild(newComp);
             $.modal.close();
         }
@@ -348,21 +338,12 @@ function createOptionAjax(event) {
         },
         success:function(response){
         if(response) {
-            let newComp= document.createElement('div');
-            newComp.setAttribute('class', 'my-6');
-            newComp.innerHTML= '<div class="flex items-center">\
-                <svg class="w-6 h-6 mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"></path></svg> \
-                <div class="text-2xl">'+comTitle+'</div> \
-                <div class="mr-8 flex mt-1" data-com-id="'+response['comId']+'"> \
-                    <button onclick="openDeleteModal(this)" class="ml-2 px-2 bg-red-500 rounded-md text-white"> \
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg> \
-                    </button> \
-                    <button onclick="openOptionEditModal(this)" class="px-2 bg-blue-500 rounded-md text-white"> \
-                        <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg> \
-                    </button> \
-                </div> \
-            </div> \
-            <div class="text-lg pr-2 py-1 border-r-2 border-gray-400" style="margin-right: 0.63rem"><!--option Value-->'+comValue+'</div>';
+            let smOptionTemplate= document.getElementsByClassName('option-template')[0];
+            let newComp= smOptionTemplate.cloneNode(true);
+            newComp.childNodes[2].childNodes[4].innerText= comTitle;
+            newComp.childNodes[4].innerText= comValue;
+            newComp.childNodes[2].childNodes[6].setAttribute('data-com-id', response['comId']);
+
             componentHolder.appendChild(newComp);
             $.modal.close();
         }
@@ -405,19 +386,10 @@ function createPureTextAjax(event) {
         },
         success:function(response){
         if(response) {
-            let newComp= document.createElement('div');
-            newComp.setAttribute('class', 'w-10/12 my-6 relative mr-1');
-            newComp.innerHTML='<svg class="w-8 h-8 absolute -mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg> \
-            <div class="flex justify-start text-lg pr-1 tracking-wide" style="padding-top: 2px;"><!--Text Content-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; \
-            '+comValue+'</div> \
-            <div class="mr-1 flex mt-1" data-com-id="'+response['comId']+'"> \
-                <button onclick="openDeleteModal(this)" class="ml-2 px-2 bg-red-500 rounded-md text-white"> \
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg> \
-                </button> \
-                <button onclick="openPureTextEditModal(this)" class="px-2 bg-blue-500 rounded-md text-white"> \
-                    <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg> \
-                </button> \
-            </div>';
+            let smOptionTemplate= document.getElementsByClassName('pure-text-template')[0];
+            let newComp= smOptionTemplate.cloneNode(true);
+            newComp.childNodes[4].innerHTML+= comValue;
+            newComp.childNodes[6].setAttribute('data-com-id', response['comId']);
             componentHolder.appendChild(newComp);
             $.modal.close();
         }
@@ -467,26 +439,11 @@ function createLinkAjax(event) {
         },
         success:function(response){
         if(response) {
-
-            let newComp= document.createElement('div');
-            newComp.setAttribute('class', 'my-6 flex items-center');
-            newComp.innerHTML='<!--link sample--><svg class="w-6 h-6 mr-1 mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg> \
-            <div class="text-2xl ml-3"><!--link title--> \
-                <span>'+comTitle+'</span>: \
-            </div> \
-            <div class="text-lg"><!--link value--> \
-                <a href="'+comValue+'"> \
-                    بازدید از لینک \
-                </a> \
-            </div> \
-            <div class="mr-8 flex mt-1" data-com-id="'+response['comId']+'"> \
-                <button onclick="openDeleteModal(this)" class="ml-2 px-2 bg-red-500 rounded-md text-white"> \
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg> \
-                </button> \
-                <button onclick="openLinkEditModal(this)" class="px-2 bg-blue-500 rounded-md text-white"> \
-                    <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg> \
-                </button> \
-            </div>';
+            let smOptionTemplate= document.getElementsByClassName('link-template')[0];
+            let newComp= smOptionTemplate.cloneNode(true);
+            newComp.childNodes[4].childNodes[2].innerText = comTitle;
+            newComp.childNodes[6].childNodes[2].setAttribute('href', comValue);
+            newComp.childNodes[8].setAttribute('data-com-id', response['comId']);
 
             componentHolder.appendChild(newComp);
             $.modal.close();
--- kntu-cms/resources/views/cvs/edit.blade.php
+++ kntu-cms/resources/views/cvs/edit.blade.php
@@ -203,6 +203,76 @@
         </div>
     </div>
 </div>
+<div hidden><!--Components Ui template Holder -->
+    <div class="flex items-center my-4 sm-option-template"><!--sm-option sample-->
+        <svg class="w-6 h-6 mt-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path></svg>
+        <div class="ml-5 text-2xl"><!--sm-option title-->
+            <span></span>:
+        </div>
+        <div class="font-semibold"><!--sm-option value-->
+        </div>
+        <div class="mr-8 flex mt-1" data-com-id="">
+            <button onclick="openDeleteModal(this)" class="ml-2 px-2 bg-red-500 rounded-md text-white">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+            </button>
+            <button onclick="openSmOptionEditModal(this)" class="px-2 bg-blue-500 rounded-md text-white">
+                <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+            </button>
+        </div>
+    </div>
+    <div class="my-6 option-template"><!--option sample-->
+        <div class="flex items-center"><!--option Title-->
+            <svg class="w-6 h-6 mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"></path></svg>
+            <div class="text-2xl"><!--title holder -->
+
+            </div>
+            <div class="mr-8 flex mt-1" data-com-id="">
+                <button onclick="openDeleteModal(this)" class="ml-2 px-2 bg-red-500 rounded-md text-white">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                </button>
+                <button onclick="openOptionEditModal(this)" class="px-2 bg-blue-500 rounded-md text-white">
+                    <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                </button>
+            </div>
+        </div>
+        <div class="text-lg pr-2 py-1 border-r-2 border-gray-400" style="margin-right: 0.63rem"><!--option Value-->
+        </div>
+    </div>
+    <div class="w-10/12 my-6 relative mr-1 pure-text-template"><!--pure_text sample-->
+        <svg class="w-8 h-8 absolute -mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
+
+        <div class="flex justify-start text-lg pr-1 tracking-wide" style="padding-top: 2px;"><!--Text Content-->
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <div class="mr-1 flex mt-1" data-com-id="">
+            <button onclick="openDeleteModal(this)" class="ml-2 px-2 bg-red-500 rounded-md text-white">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+            </button>
+            <button onclick="openPureTextEditModal(this)" class="px-2 bg-blue-500 rounded-md text-white">
+                <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+            </button>
+        </div>
+    </div>
+    <div class="my-6 flex items-center link-template"><!--link sample-->
+        <svg class="w-6 h-6 mr-1 mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
+        <div class="text-2xl ml-3"><!--link title-->
+            <span>{{$component->title}}</span>:
+        </div>
+        <div class="text-lg"><!--link value-->
+            <a href="{{$component->value}}">
+                بازدید از لینک
+            </a>
+        </div>
+        <div class="mr-8 flex mt-1" data-com-id="{{$component->id}}">
+            <button onclick="openDeleteModal(this)" class="ml-2 px-2 bg-red-500 rounded-md text-white">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+            </button>
+            <button onclick="openLinkEditModal(this)" class="px-2 bg-blue-500 rounded-md text-white">
+                <svg class="w-6 h-6 mx-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+            </button>
+        </div>
+    </div>
+</div>
 
 <script src="{{asset('js/cv/editCvAjax.js')}}">
 </script>
